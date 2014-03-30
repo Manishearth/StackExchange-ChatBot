823998d08c3419ad3379e20c5d381a8eb9b239ce
Manish Goregaokar <manishsmail@gmail.com>
2014-03-30 17:12:33 +0530

    structure

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ StackExchange-ChatBot
 =====================
 
 Generic Stack Exchange chatbot, using ChatExchange
+
+
+Still under development, and not tested
--- SEChatBot.py
+++ SEChatBot.py
@@ -3,6 +3,7 @@ from ChatExchange.SEChatWrapper import *
 class SEChatBot:
   def __init__(self, site="SE",trigger="!!"):
     self.wrap=SEChatWrapper(site)
+    self.watchers=[]
   def login(self, username, password):
     self.wrap.login(username,password)
   def joinRoom(roomno):
@@ -11,3 +12,5 @@ class SEChatBot:
     "Watch for a command, handler should take (args,roomnumber) as arguments and return a reply"
   def postMessage(roomno,message):
     "Expose inner postMessage here"
+  def _watcher():
+    "Use array of watch functions to watch room"
